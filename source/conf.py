21912fda0718c9f4ef935de38de711384b9790a0
Michael Droettboom <mdboom@gmail.com>
2013-09-06 17:30:12 -0400

    Change theme again

--- source/conf.py
+++ source/conf.py
@@ -110,7 +110,7 @@ html_theme = 'bootstrap'
 # https://github.com/ryan-roemer/sphinx-bootstrap-theme
 
 html_theme_options = {
-    'bootswatch_theme': 'simplex'
+    'bootswatch_theme': 'flatly'
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
