483ba1ababd2dab5b3bb0891969ce0136f39b506
Michael Droettboom <mdboom@gmail.com>
2013-10-15 17:24:49 -0400

    Add comments to install-texlive.sh script

--- install-texlive.sh
+++ install-texlive.sh
@@ -2,11 +2,16 @@
 
 set -e
 
+# Put the current working directory into the profile file
 sed 's@PWD@'"$PWD"'@' < texlive.profile > texlive.localized.profile
+
+# Get the TexLive installer
 wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
 tar zvxf install-tl-unx.tar.gz
 cd `ls -d install-tl-[0-9]*`
 ./install-tl --profile ../texlive.localized.profile
 cd ..
+
+# Now install the required packages into our fresh installation
 export PATH=$PWD/texlive/bin/x86_64-linux:$PATH
 tlmgr install cmap ec fancybox titlesec framed fancyvrb threeparttable mdwtools wrapfig parskip url multirow inconsolata cabin bbding microtype changepage xcolor mdframed l3packages etoolbox needspace pgf times upquote helvetic fontaxes mweights
